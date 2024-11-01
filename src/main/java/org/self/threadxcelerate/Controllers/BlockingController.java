4749fc525f03af276a69a6562d7be5a86ec1f480
Madhan-ms-21 <madhumadhan303@gmail.com>
2024-11-01 08:58:38 +0530

    Created End Points

---
+++ src/main/java/org/self/threadxcelerate/Controllers/BlockingController.java
@@ -0,0 +1,31 @@
+package org.self.threadxcelerate.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/Blocking")
+public class BlockingController {
+
+    @GetMapping("/customers")
+    public String customers() {
+        return "Customers";
+    }
+
+    @PostMapping("/customer")
+    public String customer() {
+        return "Customers";
+    }
+
+    @GetMapping("/fileRead")
+    public String fileRead() {
+        return "FileRead";
+    }
+
+    @PostMapping("/fileWrite")
+    public String fileWrite() {
+        return "FileWrite";
+    }
+}
