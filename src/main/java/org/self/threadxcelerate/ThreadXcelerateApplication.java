6d6d57e32b2468597a12be4484745d9373f73746
Madhan-ms-21 <madhumadhan303@gmail.com>
2024-11-01 08:20:08 +0530

    Repo Initialized

---
+++ src/main/java/org/self/threadxcelerate/ThreadXcelerateApplication.java
@@ -0,0 +1,13 @@
+package org.self.threadxcelerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadXcelerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadXcelerateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/self/threadxcelerate/ThreadXcelerateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.self.threadxcelerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadXcelerateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
